34041ea314c09165e84d2e570d919fb65366c999
Luis Tembe <luistembe.lt@gmail.com>
2024-01-10 15:36:34 +0200

    Adicionadas as correcoes ortograficas e estruturacao melhorada

--- src_primeira_avaliacao/script/script.js
+++ src_primeira_avaliacao/script/script.js
@@ -1,7 +1,8 @@
 //PIN sorteado
 var pin = 4444;
 
-//Funcao do botao limpar
+
+//Função para captura do número
 function leNumero(){
 var valorEntrada = document.getElementById('numero').value;
 var text;
@@ -12,34 +13,35 @@ if(isNaN(valorEntrada) || valorEntrada.length < 4){
     alert('Entrada Invalida');
 }
 else if(valorEntrada < pin){
-    document.getElementById('resultado').innerHTML = 'Experimente um numero maior.';
+    document.getElementById('resultado').innerHTML = 'Experimente um número maior.';
     document.getElementById('resultado').style.color ='#E3FC00';
-    alert('Ops! O PIN inserido nao eh o certo');
+    alert('Ops! O PIN inserido não é o certo');
 }
 else if(valorEntrada > pin){
-    document.getElementById('resultado').innerHTML = 'Experimente um numero menor.';
+    document.getElementById('resultado').innerHTML = 'Experimente um número menor.';
     document.getElementById('resultado').style.color ='#E3FC00'; 
-    alert('Ops! O PIN inserido nao eh o certo');       
+    alert('Ops! O PIN inserido não é o certo');       
 }
 else{
     text = valorEntrada;
-    document.getElementById('resultado').innerHTML = 'Parabens! O PIN '+valorEntrada+' é o correto.';
+    document.getElementById('resultado').innerHTML = 'Parabéns! O PIN '+valorEntrada+' é o correto.';
     document.getElementById('resultado').style.color ='#46BD06';
 }
 }
 
-//Funcao do botao limpar
+////Função do botao limpar
 function limpar(){
     document.getElementById('resultado').innerHTML ='';
     document.getElementById('numero').value ='';
 }
 
+//Texo alternado sobre o prorgama
 setInterval (function(){
-    document.body.children[1].textContent = 'Numero de Identificacao Pessoal'
+    document.body.children[1].textContent = 'Número de Identificação Pessoal'
     document.body.children[1].style.color = '#eef20a'
     }, 4000);
 
 setInterval (function(){
-    document.body.children[1].textContent = 'Avaliação Sprint 1'
+    document.body.children[1].textContent = 'Avaliação da Sprint 1'
     document.body.children[1].style.color = '#13e848'
     }, 8000);
